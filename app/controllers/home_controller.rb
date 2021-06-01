2ad47a3ba2e5e300870cc0ec6cc62bb46655360a
Roelly Wang <a123458524@yahoo.com.tw>
2021-06-01 16:53:34 +0800

    做到一半

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_6ec2e624bf2b4acabb637ff52176e428')
+    # StockQuote::Stock.new(api_key: 'pk_6ec2e624bf2b4acabb637ff52176e428')
+    
+    if params[:ticker] == ""
+      @nothing = "Don't forget enter the symbol!"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        @logo = StockQuote::Stock.logo(params[:ticker])
+      rescue StandardError
+        @nothing = "Please check the symbol you enter!"
+      end
+    end
+  end
+
+  def about
   end
 end
+
+script
